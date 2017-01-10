ee1fc22d3a3ef315a28c335ec38bd248fa16a0e8
Rhee <clq1@qq.com>
2017-01-11 00:55:12 +0800

    用户列表

--- src/components/nav1/userList.vue
+++ src/components/nav1/userList.vue
@@ -5,33 +5,32 @@
             <el-form :inline="true" :model="form" class="demo-form-inline" label-width="90px"
                      @submit.prevent="onSubmit">
                 <!--<el-form-item>-->
-                    <!--<el-input v-model="formInline.user" placeholder="姓名"></el-input>-->
+                <!--<el-input v-model="formInline.user" placeholder="姓名"></el-input>-->
                 <!--</el-form-item>-->
                 <!--<el-form-item>-->
-                    <!--<el-button type="primary">查询</el-button>-->
+                <!--<el-button type="primary">查询</el-button>-->
                 <!--</el-form-item>-->
                 <!--<el-form-item>-->
-                    <!--<el-button @click="handleAdd">新增</el-button>-->
+                <!--<el-button @click="handleAdd">新增</el-button>-->
                 <!--</el-form-item>-->
                 <el-form-item label="注册时间：">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1"
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.registerDateStart"
                     ></el-date-picker>
-
-                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date2"
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.registerDateEnd"
                     ></el-date-picker>
                 </el-form-item>
                 <el-form-item label="品牌/车型：">
-                    <el-select v-model="form.region1" placeholder="请选择活动区域">
+                    <el-select v-model="form.brandName" placeholder="请选择活动区域">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
-                    <el-select v-model="form.region2" placeholder="请选择活动区域">
+                    <el-select v-model="form.brandType" placeholder="请选择活动区域">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
                 </el-form-item>
                 <el-form-item label="用户来源：">
-                    <el-select v-model="form.source" placeholder="请选择活动区域">
+                    <el-select v-model="form.registerSource" placeholder="请选择活动区域">
                         <el-option label="区域一" value="shanghai"></el-option>
                         <el-option label="区域二" value="beijing"></el-option>
                     </el-select>
@@ -47,22 +46,27 @@
                     </el-select>
                 </el-form-item>
                 <el-form-item>
-                    <el-input placeholder="请输入内容" v-model="input5">
-                        <el-select v-model="query" slot="prepend" placeholder="请选择">
+                    <el-input placeholder="请输入内容" v-model="realname">
+                        <el-select v-model="mobilephone" slot="prepend" placeholder="请选择" class="el-select-w">
                             <el-option label="客户名称" value="1"></el-option>
                             <el-option label="手机号" value="2"></el-option>
                         </el-select>
                     </el-input>
                 </el-form-item>
                 <el-form-item label="是否可用：">
-                    <el-select v-model="form.source" placeholder="请选择">
+                    <el-select v-model="form.status" placeholder="请选择">
                         <el-option label="是" value="shanghai"></el-option>
                         <el-option label="否" value="beijing"></el-option>
                     </el-select>
                 </el-form-item>
+                <el-col :span="24">
+                    <el-form-item class="el-submit">
+                        <el-button type="primary" icon="search">查询</el-button>
+                        <el-button @click="handleAdd">新增</el-button>
+                    </el-form-item>
+                </el-col>
             </el-form>
         </el-col>
-
         <!--列表-->
         <template>
             <el-table :data="tableData" highlight-current-row v-loading="listLoading" style="width: 100%;">
@@ -86,7 +90,6 @@
                 </el-table-column>
             </el-table>
         </template>
-
         <!--分页-->
         <el-col :span="24" class="toolbar" style="padding-bottom:10px;">
             <el-pagination :current-page="1" :page-sizes="[100, 200, 300, 400]" :page-size="100"
@@ -94,7 +97,6 @@
                            :total="400" style="float:right">
             </el-pagination>
         </el-col>
-
         <!--编辑界面-->
         <el-dialog :title="editFormTtile" v-model="editFormVisible" :close-on-click-modal="false">
             <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
@@ -128,25 +130,23 @@
         </el-dialog>
     </section>
 </template>
-
 <script>
 	import util from '../../common/util'
 	import NProgress from 'nprogress'
-
   export default {
     data() {
       return {
 				form: {
-					user: '',
-					date1: '',
-					date2: '',
-					region1: '',
-					region2: '',
-					source: '',
-					input5: '',
-					query:'',
+					registerDateStart: '',
+					registerDateEnd: '',
+					brandName: '',
+					brandType: '',
+					registerSource: '',
+					realname: '',
+					mobilephone :'',
 					province:'',
-					city:''
+					city:'',
+					status:''
 				},
 				pickerOptions0: {
 					disabledDate(time) {
@@ -250,7 +250,6 @@
 						for(var i=0;i<_this.tableData.length;i++){
 							if(_this.tableData[i].id==row.id){
 								_this.tableData.splice(i,1);
-
 								_this.listLoading=false;
 								NProgress.done();
 								_this.$notify({
@@ -258,13 +257,11 @@
 									message: '删除成功',
 									type: 'success'
 								});
-
 								break;
 							}
 						}
 					},1000);
 				}).catch(() => {
-
 				});
 			},
 			//显示编辑界面
@@ -281,10 +278,8 @@
 			//编辑 or 新增
 			editSubmit:function(){
 				var _this=this;
-
 				_this.$refs.editForm.validate((valid)=>{
 					if(valid){
-
 						_this.$confirm('确认提交吗？','提示',{}).then(()=>{
 							_this.editLoading=true;
 							NProgress.start();
@@ -299,7 +294,6 @@
 									type: 'success'
 								});
 								_this.editFormVisible = false;
-
 								if(_this.editForm.id==0){
 									//新增
 									_this.tableData.push({
@@ -324,20 +318,15 @@
 									}
 								}
 							},1000);
-
 						});
-
 					}
 				});
-
 			},
 			//显示新增界面
 			handleAdd:function(){
 				var _this=this;
-
 				this.editFormVisible=true;
 				this.editFormTtile='新增';
-
 				this.editForm.id=0;
 				this.editForm.name='';
 				this.editForm.sex=1;
@@ -349,21 +338,20 @@
   }
 
 
-</script>
 
+</script>
 <style scoped>
 	.toolbar .el-form-item {
 		margin-bottom: 10px;
 	}
-
 	.toolbar {
 		background: #fff;
 		padding: 10px 10px 0px 10px;
 	}
-
-	.el-form-item__content{
-	    margin:0;
-	}
-
+	.el-date-editor.el-input {width: 217px;}
+    .el-select-w{width:120px}
+    .el-input{width:440px}
+    .el-submit{width:335px;margin:10px auto;display:block}
 
 </style>
+
